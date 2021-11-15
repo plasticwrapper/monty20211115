2da3c1f59e529abe61d2aba0141bdcd958702564
Donald Hall <donnie@plasticwrapper.com>
2021-11-15 04:34:30 -0600

    ugly version working, 10000 example in specs, rubocop added but should have been in a separate commit

--- lib/monty.rb
+++ lib/monty.rb
@@ -1,33 +1,44 @@
 class Monty
-  attr_accessor :doors
+  attr_accessor :doors, :result
 
   # assume the user always switch doors
   def initialize
-
     # setup the game
-    @doors = [:car, :goat, :goat]
+    @doors = %i[car goat goat].shuffle
 
-    # user picks a door (doesn't matter which)
-    pick_a_door
+    # user picks a door
+    @pick1 = pick_a_door
 
     # monty reveals a goat
-    reveal_a_goat
+    @goat1 = reveal_a_goat
 
-    # user picks different remaining door
-    switch_doors
+    # user switches doors
+    @pick2 = pick_remaining_door
+
+    @result = (@doors[@pick2] == :car)
   end
 
   private
 
+  # return the position of the user's first pick
   def pick_a_door
-
+    rand(0..2)
   end
 
+  # return the position of one goat
   def reveal_a_goat
-
+    remaining_doors = [0, 1, 2]
+    remaining_doors.delete_at(@pick1)
+    remaining_doors.delete_at(@doors.index(:car))
+    remaining_doors.first
   end
 
-  def switch_doors
-
+  # return the position of the remaining door
+  # FIXME: DRY this up... note the design flaw..
+  def pick_remaining_door
+    remaining_doors = [0, 1, 2]
+    remaining_doors.delete_at(@pick1)
+    remaining_doors.delete_at(@goat1)
+    remaining_doors.first
   end
 end
