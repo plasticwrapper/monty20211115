a595bad2b57c18768900b2988c08567c004117ea
Donald Hall <donnie@plasticwrapper.com>
2021-11-15 02:50:43 -0600

    adding some placeholder private methods for breaking down each step

--- lib/monty.rb
+++ lib/monty.rb
@@ -7,13 +7,27 @@ class Monty
     # setup the game
     @doors = [:car, :goat, :goat]
 
-    # user picks a door
+    # user picks a door (doesn't matter which)
+    pick_a_door
 
     # monty reveals a goat
+    reveal_a_goat
 
     # user picks different remaining door
+    switch_doors
   end
 
   private
 
+  def pick_a_door
+
+  end
+
+  def reveal_a_goat
+
+  end
+
+  def switch_doors
+
+  end
 end
